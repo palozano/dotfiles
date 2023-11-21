c6e7fcb91a33d93c57911c4990b1a4acf28cf73f
Pablo Lozano <nivolabs@proton.me>
2023-11-21 12:33:46 +0100

    feat: mainly emacs

---
+++ config/emacs/custom-vars.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck company lsp-mode rustic key-chord which-key diminish counsel-projectile nerd-icons-ivy-rich counsel nerd-icons-corfu corfu diff-hl magit nerd-icons-ibuffer nerd-icons-dired eat org-bullets toc-org yasnippet-snippets projectile doom-modeline gruvbox-theme general evil-collection evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- setup.sh
+++ setup.sh
@@ -10,13 +10,17 @@ set -x
 mkdir -p ~/.config
 
 # Link Git config if it doesn’t exist
-[ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
+# [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
+ln -sf "$PWD/config/git" ~/.config/git
 
 # Add the Zsh config file if it doesn’t exist
-[ ! -e ~/.zshrc ] && ln -s "$PWD/config/zshrc" ~/.zshrc
+# [ ! -e ~/.zshrc ] && ln -s "$PWD/config/zshrc" ~/.zshrc
+ln -sf "$PWD/config/zshrc" ~/.zshrc
 
-# Add the neovim config file if it doesn’t exist
-[ ! -e ~/.config/nvim ] && ln -s "$PWD/config/nvim" ~/.config/nvim
+# Add the neovim config if it doesn’t exist
+# [ ! -e ~/.config/nvim ] && ln -s "$PWD/config/nvim" ~/.config/nvim
+ln -sf "$PWD/config/nvim" ~/.config/nvim
 
-# Add the emacs config file if it doesn’t exist
-[ ! -e ~/.config/emacs ] && ln -s "$PWD/config/emacs" ~/.config/emacs
+# Add the emacs config if it doesn’t exist
+# [ ! -e ~/.config/emacs ] && ln -s "$PWD/config/emacs" ~/.config/emacs
+ln -sf "$PWD/config/emacs" ~/.config/emacs
