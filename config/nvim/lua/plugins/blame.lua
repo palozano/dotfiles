2edcc499fd2b0d133d4d4dfa66ab209c125bf99c
Pablo Lozano <nivolabs@proton.me>
2024-01-26 15:37:35 +0100

    chore: faster, cleaner blames

--- config/nvim/lua/plugins/blame.lua
+++ config/nvim/lua/plugins/blame.lua
@@ -3,7 +3,8 @@ return {
   opts = {
     virt_text = true,
     virt_text_pos = 'eol',
-    delay = 1000,
+    delay = 300,
     date_format = '%r',
+    message_template = ' <author>, <date>, <summary>',
   }
 }
